8e01e0c578d412cc8a1cd508e68bb34ca6e48333
NailSacul <ianlucaschaves@gmail.com>
2023-11-28 12:50:38 -0300

    Update

--- src/main/java/com/naisacul/challengeeventsmicroservice/ChallengeEventsMicroserviceApplication.java
+++ src/main/java/com/naisacul/challengeeventsmicroservice/ChallengeEventsMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.naisacul.challengeeventsmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ChallengeEventsMicroserviceApplication {
 
 	public static void main(String[] args) {
