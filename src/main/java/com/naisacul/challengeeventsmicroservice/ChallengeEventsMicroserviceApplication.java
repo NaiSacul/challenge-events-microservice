2626f891d3f518ac6b62db2067ba627fdde135ab
NailSacul <ianlucaschaves@gmail.com>
2023-11-27 23:09:09 -0300

    Project created

---
+++ src/main/java/com/naisacul/challengeeventsmicroservice/ChallengeEventsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.naisacul.challengeeventsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeEventsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeEventsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naisacul/challengeeventsmicroservice/ChallengeEventsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naisacul.challengeeventsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeEventsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
