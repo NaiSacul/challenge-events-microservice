b9a366fb75d1ba0a36343e65b65820d1a8cee219
NailSacul <ianlucaschaves@gmail.com>
2023-11-28 12:44:55 -0300

    Services

---
+++ src/main/java/com/naisacul/challengeeventsmicroservice/services/EmailServiceClient.java
@@ -0,0 +1,13 @@
+package com.naisacul.challengeeventsmicroservice.services;
+
+import com.naisacul.challengeeventsmicroservice.dtos.EmailRequestDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "email-service", url = "http://endereco-do-servico-de-email")
+public interface EmailServiceClient {
+
+    @PostMapping("/send")
+    void sendEmail(@RequestBody EmailRequestDTO emailRequest);
+}
